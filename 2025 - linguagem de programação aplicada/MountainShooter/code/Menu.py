5fc550f9e0e8478451c7576964b96356056d8a0a
Daniela Nogueira Rampim <daninogueira.dev@gmail.com>
2025-04-14 22:06:34 -0300

    :sparkles: Creating the menu: background, sound and fonts :sparkles: Creating the Const file

---
+++ 2025 - linguagem de programação aplicada/MountainShooter/code/Menu.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import pygame
+from pygame.font import Font
+from pygame.rect import Rect
+from pygame.surface import Surface
+
+from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE
+
+
+class Menu:
+    def __init__(self, window):
+        self.window = window
+        # imagens
+        # importando a imagem
+        self.surf = pygame.image.load('./asset/MenuBg.png')
+
+        # desenhando o retangulo para a imagem
+        self.rect = self.surf.get_rect(left=0, top=0)
+
+    def run(self):
+        # importando o som do menu
+        pygame.mixer_music.load('./asset/Menu.mp3')
+
+        # tocando a musica infinita
+        pygame.mixer_music.play(-1)
+
+        while True:
+            # direcionando de onde vem a imagem e onde ela vai
+            self.window.blit(source=self.surf, dest=self.rect)
+
+            # escrevendo o texto
+            self.menu_text(50, "Mountain", COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Shooter", COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(25, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 180 + 30 * i))
+
+            # atualizando o display
+            pygame.display.flip()
+
+            # Check for all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # Close window
+                    quit()  # end pygame
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        # metodo para desenhar o texto via pygame
+
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
