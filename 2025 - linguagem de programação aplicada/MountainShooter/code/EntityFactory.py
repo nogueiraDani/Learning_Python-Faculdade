589fa1fc1f75e4f99475b94fa445bbf3ee01df7b
Daniela Nogueira Rampim <daninogueira.dev@gmail.com>
2025-04-22 21:15:37 -0300

    :sparkles: creating menu interations, creating ABC class, creating Factory, Level1Bg implementation: parallax

--- 2025 - linguagem de programação aplicada/MountainShooter/code/EntityFactory.py
+++ 2025 - linguagem de programação aplicada/MountainShooter/code/EntityFactory.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from typing import Any
 
+from code.Background import Background
+from code.Const import WIN_WIDTH
+
+
+# entidade Fabrica resposavel por gerar player, enemy e background
 class EntityFactory:
-    def __init__(self):
-        pass
-
-    def get_entity(self, entity_type):
-        pass
+# na factory nao existe __init__
+    @staticmethod
+    def get_entity(entity_name: str):
+        match entity_name:
+            case 'Level1Bg':
+                list_bg = []
+                for i in range(7):
+                    list_bg.append(Background(f'Level1Bg{i}', (0,0)))
+                    list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH,0)))
+                return list_bg
+        return None
--- 2025 - linguagem de programação aplicada/MountainShooter/code/Menu.py
+++ 2025 - linguagem de programação aplicada/MountainShooter/code/Menu.py
@@ -5,7 +5,7 @@ from pygame.font import Font
 from pygame.rect import Rect
 from pygame.surface import Surface
 
-from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE
+from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE, COLOR_YELLOW
 
 
 class Menu:
@@ -19,6 +19,8 @@ class Menu:
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self):
+        menu_option = 0
+
         # importando o som do menu
         pygame.mixer_music.load('./asset/Menu.mp3')
 
@@ -34,7 +36,10 @@ class Menu:
             self.menu_text(50, "Shooter", COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
-                self.menu_text(25, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 180 + 30 * i))
+                if i == menu_option:
+                    self.menu_text(25, MENU_OPTION[i], COLOR_YELLOW, ((WIN_WIDTH / 2), 180 + 30 * i))
+                else:
+                    self.menu_text(25, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 180 + 30 * i))
 
             # atualizando o display
             pygame.display.flip()
@@ -44,6 +49,20 @@ class Menu:
                 if event.type == pygame.QUIT:
                     pygame.quit()  # Close window
                     quit()  # end pygame
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DOWN:
+                        if menu_option < len(MENU_OPTION) - 1:
+                            menu_option += 1
+                        else:
+                            menu_option = 0
+                    if event.key == pygame.K_UP:
+                        if menu_option > 0:
+                            menu_option -= 1
+                        else:
+                            menu_option = len(MENU_OPTION) - 1
+                    if event.key == pygame.K_RETURN:
+                        return MENU_OPTION[menu_option]
+
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         # metodo para desenhar o texto via pygame
